835a885d0598beb046b67dda54ca0e07b584b251
mrpyrex <ndifrkeumoren@gmail.com>
2020-04-12 09:59:07 +0100

    Updated uri

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ApolloProvider, Query } from "react-apollo";
 import ApolloClient, { gql } from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql/",
+  uri: "https://treasurehousebackend.herokuapp.com/graphql/",
 
   fetchOptions: {
     credentials: "include",
